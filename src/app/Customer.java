a99e2643edbc4efc19b6f12a33c9c50511507041
undefinedDen <tunevdenis123@gmail.com>
2025-02-11 23:15:55 +0200

    Add costructor, getters, setters

--- src/app/Customer.java
+++ src/app/Customer.java
@@ -1,4 +1,27 @@
 package app;
 
 public class Customer {
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
