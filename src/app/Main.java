e395576e3ee5083c26e8f642a6ab4717e1f4aa77
undefinedDen <tunevdenis123@gmail.com>
2025-02-12 00:16:05 +0200

    Create classic program`s variant with Array

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,17 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-
+Customer customer = getCustomer(getData());
+String output = "Customer: " + customer.getName() + ", phone: " + customer.getPhone();
+getOutput(output);
+    }
+    public static void getOutput(String output){
+        System.out.println(output);
+    }
+    public static String[] getData(){
+        return new String[]{"Alice", "0680256-512"};
+    }
+    public static Customer getCustomer(String[] data){
+        return new Customer(data[0], data[1]);
     }
 }
